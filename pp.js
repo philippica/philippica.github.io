4e10aae601a238d4b512930ddfe4a0d312968b88
philippica <429844547@qq.com>
2015-10-26 15:17:21 +0800

    Update pp.js

--- pp.js
+++ pp.js
@@ -1,51 +1,107 @@
+var ppMode = "paint";
 var ppPoint = function(x, y)
 {
 	this.x = x;
 	this.y = y;
 }
-var ppPointArray = new Array();
+var ppLastPoint;
+var ppInitialPoint;
 var ppMousePressed;
 var context = document.getElementById('myCanvas').getContext("2d");
+var ppCanvasWidth = $('#myCanvas').width();
+var ppCanvasHeight = $('#myCanvas').height();
+var ppImgData;
+function test()
+{
+	var ppImgData = context.getImageData(0, 0, ppCanvasWidth, ppCanvasHeight);
+	//context.clearRect(0, 0, ppCanvasWidth, ppCanvasHeight);
+	context.putImageData(ppImgData, 100, 0);
+}
 
 
+function ppSetModeDrag()
+{
+	ppMode = "drag";
+}
+function ppSetModePaint()
+{
+	ppMode = "paint";
+}
 function ppDrawLine(curX, curY)
 {
 	context.beginPath();
-	var ppLastPoint = ppPointArray[ppPointArray.length - 1];
 	context.moveTo(ppLastPoint.x, ppLastPoint.y);
 	context.lineTo(curX, curY);
 	context.closePath();
 	context.stroke();
-	ppPointArray.push(new ppPoint(curX, curY));
+	ppLastPoint = new ppPoint(curX, curY);
 }
 
 function ppCanvasInit()
 {
 	context.strokeStyle = "red";
 	context.lineJoin = "round";
-	context.lineWidth = 1;
+	context.lineWidth = 5;
 }
 
 
-$('#myCanvas').mouseleave(function(e){
-	if(ppMousePressed)
+function ppDrag(curX, curY)
+{
+	var dX = curX - ppInitialPoint.x;
+	var dY = curY - ppInitialPoint.y;
+	console.info(dX+" "+dY);
+	context.putImageData(ppImgData, dX, dY);
+}
+
+function ppOpenFile(ppFile)
+{
+	var image = new Image();
+	image.src = "1-1401261H42XJ.jpg";
+	image.onload = function()
 	{
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-		ppDrawLine(mouseX, mouseY);
+		context.drawImage(image, 0, 0);
+	}
+	alert(ppFile.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+$('#myCanvas').mouseleave(function(e)
+{
+	if(ppMode == "paint")
+	{
+		if(ppMousePressed)
+		{
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			ppDrawLine(mouseX, mouseY);
+		}
 	}
 });
 
-$('#myCanvas').mouseenter(function(e){
-	var mouseX = e.pageX - this.offsetLeft;
-	var mouseY = e.pageY - this.offsetTop;
-	ppPointArray.push(new ppPoint(mouseX, mouseY));
+$('#myCanvas').mouseenter(function(e)
+{
+	if(ppMode == "paint")
+	{
+		var mouseX = e.pageX - this.offsetLeft;
+		var mouseY = e.pageY - this.offsetTop;
+		ppLastPoint = new ppPoint(mouseX, mouseY);
+	}
 });
 
 
 
 
 $(window).mouseup(function(e){
+
 	ppMousePressed = false;
 });
 
@@ -54,20 +110,42 @@ $('#myCanvas').mousedown(function(e)
 	if(e.which == 1)
 	{
 		ppMousePressed = true;
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-		ppPointArray.push(new ppPoint(mouseX, mouseY));
+		if(ppMode == "paint")
+		{
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			ppLastPoint = new ppPoint(mouseX, mouseY);
+		}
+		if(ppMode == "drag")
+		{
+			ppImgData = context.getImageData(0, 0, ppCanvasWidth, ppCanvasHeight);
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			ppInitialPoint = new ppPoint(mouseX, mouseY);
+		}
 	}
 });
+
+
 
 
 $('#myCanvas').mousemove(function(e)
 {
 	if(ppMousePressed)
 	{
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-		ppDrawLine(mouseX, mouseY);
+		if(ppMode == "paint")
+		{
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			ppDrawLine(mouseX, mouseY);
+		}
+		if(ppMode == "drag")
+		{
+			var mouseX = e.pageX - this.offsetLeft;
+			var mouseY = e.pageY - this.offsetTop;
+			context.clearRect(0, 0, ppCanvasWidth, ppCanvasHeight);
+			ppDrag(mouseX, mouseY);
+		}
 	}
 });
 
